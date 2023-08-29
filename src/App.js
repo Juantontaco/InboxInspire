79262490e7732d732bb9f05e0917a13362260ccd
juantontaco <juantontaco@gmail.com>
2023-08-29 18:14:19 -0400

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/details/:id" component={NewsletterDetails} />
+        {/* Add other routes as needed */}
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
